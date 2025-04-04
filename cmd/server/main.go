3edd15fde9a813ceab16238e8245dc664bf38248
Felipe Lima <flima1910@gmail.com>
2025-04-04 20:01:06 -0300

    feat: add gorilla depedency

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dunnodwarf/proglog/internal/server"
 	"log"
 )
 
 func main() {
+	fmt.Println("stop here")
 	srv := server.NewHttpServer(":8080")
 	log.Fatal(srv.ListenAndServe())
 }
